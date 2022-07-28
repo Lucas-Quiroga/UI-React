548174df2a2ffdc5bb3efb8c01af134cbd870f91
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-07-28 17:55:26 -0300

    components.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./Components/Login/Login";
+import Landing from "./Components/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Landing />
     </div>
   );
 }
